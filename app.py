0843de6a8f928ce6d55043c1329184aa1e6b2683
Ricardo Perez <ricardopsf1@gmail.com>
2022-10-31 15:43:00 -0300

    first commit

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, jsonify
+
+app = Flask( __name__ )
+
+from data import todos
+
+@app.route("/todos")
+def getTodos():
+    return jsonify(todos)
+
+@app.route("/todos/<string:id_todo>")
+def getTodo(id_todo):
+    todo = [todo for todo in todos if todo['id'] == id_todo]
+    return todo
+
+if __name__ == '__main__':
+    app.run( debug = True, port = 4000 )
---
+++ data.py
@@ -0,0 +1,30 @@
+todos = [
+    {
+        'id':'1',
+        'do': 'Ejercicio',
+        'creation': 3110221354,
+        'tofinish': 3110221500,
+        'type': 'Ejercicios'
+    },
+    {
+        'id':'2',
+        'do': 'Programar',
+        'creation': 2210221554,
+        'tofinish': 3110221500,
+        'type': 'Estudios'
+    },
+    {
+        'id':'3',
+        'do': 'Cuidar a la Emma',
+        'creation': 3110221500,
+        'tofinish': 3110221800,
+        'type': 'Familia'
+    },
+    {
+        'id':'4',
+        'do': 'Celebrar Halloween, pedir dulces',
+        'creation': 3110221454,
+        'tofinish': 3110222200,
+        'type': 'Fiesta'
+    }
+]
